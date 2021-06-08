1dddcb1e6c7bd07c9495233ff56147e56f04b80a
Walid Oulderra <38976111+Mysticboi@users.noreply.github.com>
2021-06-08 14:56:28 +0200

    barCode

--- App/screens/Profile.js
+++ App/screens/Profile.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text, StatusBar } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 import { Avatar } from 'react-native-elements';
 
@@ -24,6 +25,19 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  barCodeContainer: {
+    flex: 1,
+    width: 400,
+    height: 400,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  barCode: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
   footer: {
     flex: 1,
     justifyContent: 'flex-end',
@@ -31,6 +45,35 @@ const styles = StyleSheet.create({
 });
 
 export default ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [barcodeActive, setBarcodeActive] = useState(false);
+  const [type, setType] = useState();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    if (!scanned) {
+      setScanned(true);
+      setType(type);
+      setData(data);
+      // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    }
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#708090" />
@@ -45,6 +88,35 @@ export default ({ navigation }) => {
       </View>
 
       <Text style={styles.welcomeText}>Hi Walid!</Text>
+      <View style={styles.barCode}>
+        <Button light onPress={() => setBarcodeActive(!barcodeActive)}>
+          <Text>
+            {barcodeActive
+              ? 'Tap here to close the scanner'
+              : 'Tap here to start scanning'}
+          </Text>
+        </Button>
+        {scanned && (
+          <Button onPress={() => setScanned(false)}>
+            <Text>Tap to scan again</Text>
+          </Button>
+        )}
+
+        {scanned && (
+          <Text>
+            Type: {type}, code: {data}
+          </Text>
+        )}
+      </View>
+
+      {barcodeActive && (
+        <View style={styles.barCodeContainer}>
+          <BarCodeScanner
+            onBarCodeScanned={handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+        </View>
+      )}
 
       <View style={styles.footer}>
         <Footer>
